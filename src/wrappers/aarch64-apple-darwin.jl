9acedabc0b46f55616ae71ca702e8894773cb8de
jlbuild <juliabuildbot@gmail.com>
2023-08-30 22:02:08 +0000

    libcifpp_jll build 5.1.1+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll)
     JLLWrappers.@init_library_product(
         libcifpp,
-        "lib/libcifpp.5.dylib",
+        "lib/libcifpp.5.1.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
